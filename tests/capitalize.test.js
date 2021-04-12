f9d52480fec1608e8410d70ac3e23de5a304943d
a.e.tarasov <a.e.tarasov@tinkoff.ru>
2021-04-12 17:43:52 +0300

    added jest

--- tests/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import { strict as assert } from 'assert';
-import capitalize from '../src/capitalize.js';
-
-assert.strictEqual(capitalize('hello'), 'Hello');
-assert.strictEqual(capitalize(''), '');
-
-console.log('Все тесты пройдены!');
